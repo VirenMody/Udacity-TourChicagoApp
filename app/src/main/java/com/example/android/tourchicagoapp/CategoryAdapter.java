e8b9e8ed8afd9242717e33d867bae037d73023a7
Viren Mody <virenmody@gmail.com>
2018-09-11 22:30:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/tourchicagoapp/CategoryAdapter.java
@@ -0,0 +1,48 @@
+package com.example.android.tourchicagoapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new SightsFragment();
+        } else if (position == 1) {
+            return new MuseumsFragment();
+        } else if (position == 2) {
+            return new RestaurantsFragment();
+        } else {
+            return new SportArenasFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_sights);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_museums);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_restaurants);
+        } else {
+            return mContext.getString(R.string.category_sportarenas);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/tourchicagoapp/MuseumsFragment.java
@@ -0,0 +1,6 @@
+package com.example.android.tourchicagoapp;
+
+import android.support.v4.app.Fragment;
+
+public class MuseumsFragment extends Fragment {
+}
